2f81ef177df83200c7cdc920deea29c945e26fa2
benjaminfloyd <benjamin.floyd@me.com>
2017-05-03 19:40:10 -0400

    1st commit

--- labs/unit_01/javaScript/in-class-click-events/script.js
+++ labs/unit_01/javaScript/in-class-click-events/script.js
@@ -1,2 +1,33 @@
 window.onload = function() {
+var button = document.getElementById('buttonOne');
+
+button.addEventListener("click",function(){
+    alert('You have clicked button 1')
+});
+
+
+var buttonTwo = document.getElementById('buttonTwo');
+
+buttonTwo.addEventListener("click",function() {
+    var par1 = document.createElement("p");
+
+
+    par1.innerHTML = 'A click event is essentially tying a function (as a callback) to an element and specifying what action needs to happen to trigger or call/invoke that function (callback)' ;
+    document.getElementById('info').appendChild(par1);
+
+});
+
+var buttonThree = document.getElementById('buttonThree');
+
+buttonThree.addEventListener("click",function() {
+    var removepar = document.getElementsByTagName('p')[0];
+    document.getElementById('info').removeChild(removepar);
+
+});
 }
+
+// // var button = document.querySelector("button");
+
+// // button.addEventListener("click", function(event){
+//   // console.log("Executed in the callback function.");
+// });
