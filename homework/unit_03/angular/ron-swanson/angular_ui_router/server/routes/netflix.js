daf317f09c1b2ac04bff7bb24fa4577b8b3be6b3
benjaminfloyd <benjamin.floyd@me.com>
2017-06-07 16:06:37 -0400

    Set up server quotes route

--- homework/unit_03/angular/ron-swanson/angular_ui_router/server/routes/netflix.js
+++ homework/unit_03/angular/ron-swanson/angular_ui_router/server/routes/netflix.js
@@ -4,4 +4,27 @@ var Netflix = require('../../models/netflix.js');
 
 //your routes here
 
+//GET
+router.get('/', function(request, response){
+    Netflix.find(function(error, netflix){
+    if(error) response.json({message: 'Could not find movie'});
+
+    response.json({netflix: netflix});
+})
+});
+
+// Post
+router.post('/', function(request, response){
+    console.log('in POST');
+    console.log('body:', request.body)
+
+    var netflix = new Netflix(request.body);
+
+    quote.save(function(error){
+        if(error) response.json({ message: 'Moive not saved' + error});
+
+        response.json({netflix: netflix});
+    });
+});
+
 module.exports = router;
--- homework/unit_03/angular/ron-swanson/angular_ui_router/server/routes/quotes.js
+++ homework/unit_03/angular/ron-swanson/angular_ui_router/server/routes/quotes.js
@@ -1,7 +1,82 @@
 var express = require('express');
 var router = express.Router();
+var methodOverride = require('method-override') 
 var Quote = require('../../models/quote.js');
 
-//your routes here
+
+
+
+// GET
+router.get('/', function(request, response) {
+  Quote.find(function(error, quotes) {
+    if(error) response.json({message: 'Could not find any quote'});
+
+    response.json({quotes: quotes});
+  });
+});
+
+// // GET
+// router.get('/', function(request, response) {
+//   Quote.find({}).exec(function(error, quotes) {
+//     if(error) { console.log(error); }
+//     console.log(quotes)
+//     response.json({quotes: quotes});
+//     //could be response.json({ quotes })
+//   })
+// });
+
+// POST
+router.post('/', function(request, response) {
+  console.log('in POST');
+  console.log('body:',request.body);
+
+  var quote = new Quote(request.body);
+
+  quote.save(function(error) {
+    if(error) response.json({messsage: 'Could not save quote b/c:' + error});
+
+    response.json({quote: quote});
+  });
+});
+
+// GET
+router.get('/:id', function(request, response) {
+  var id = request.params.id;
+
+  Quote.findById({_id: id}, function(error, quote) {
+    if(error) response.json({message: 'Could not find quote b/c:' + error});
+
+    response.json({quote: quote});
+  }).select('-__v');
+});
+
+router.patch('/:id', function(request, response) {
+  var id = request.params.id;
+
+  Quote.findById({_id: id}, function(error, quote) {
+    if(error) response.json({message: 'Could not find quote b/c:' + error});
+
+    if(request.body.name) quote.name = request.body.name;
+    if(request.body.location) quote.location = request.body.location;
+    if(request.body.status) quote.status = request.body.status;
+
+    quote.save(function(error) {
+      if(error) response.json({messsage: 'Could not update quote b/c:' + error});
+
+      response.json({message: 'quote successfully updated', quote: quote});
+    });
+  }).select('-__v');
+});
+
+
+router.delete('/:id', function(request, response) {
+  var id = request.params.id;
+
+  Quote.remove({_id: id}, function(error) {
+    if(error) response.json({message: 'Could not delete quote b/c:' + error});
+
+    response.json({message: 'Quote successfully deleted'});
+  }).select('-__v');
+});
 
 module.exports = router;
